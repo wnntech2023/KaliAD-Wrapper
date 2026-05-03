752e489fad2b39d5034866c8c15cfae2a7b20843
wnntech2023 <nepomnyu.danya@bk.ru>
2026-05-03 17:28:03 +0300

    Update enum4linux.py

--- modules/enum4linux.py
+++ modules/enum4linux.py
@@ -2,8 +2,16 @@ from config import CONFIG
 from utils.runner import run_tool
 
 def parse_enum(out: str):
-    users = out.count("user:")
-    return {"summary": f"Найдено пользователей: {users}"}
+    users = out.count("user(s)") + out.count("Found 15 user") + out.count("username:")
+    groups = out.count("group(s)") + out.count("Found 55 group") + out.count("groupname:")
+    shares = out.count("share(s)")
+    summary = f"Пользователей: {users} | Групп: {groups} | Шар: {shares}"
+    return {
+        "users_found": users,
+        "groups_found": groups,
+        "shares_found": shares,
+        "summary": summary
+    }
 
 def run_enum4linux_check():
     cmd = ["enum4linux-ng", "-A", "-u", CONFIG['username'], "-p", CONFIG['password'], CONFIG['dc_ip']]
